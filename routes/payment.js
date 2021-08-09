7ea81ce3cdb5414fcb434b5d9e290d7f782fa7ec
Frédéric <desseaux_frederic@yahoo.fr>
2021-08-09 13:26:31 +0200

    dotenv added

--- routes/payment.js
+++ routes/payment.js
@@ -2,12 +2,14 @@ const express = require("express");
 const formidable = require("express-formidable");
 const router = express.Router();
 const isAuthenticated = require("../middlewares/isAuthenticated");
-const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const stripe = require("stripe")(
+  "sk_test_51JLodcFTc2lW5LhwN3UXU466ouanDJSoRTFpFb3rbepL584zXyzUpoMR9pQiTO1EP0dilXDnwc65xW9mC7DOQxqv00dvx9XHkV"
+);
 
 router.post("/payment", isAuthenticated, async (req, res) => {
   try {
     const stripeToken = req.fields.stripeToken;
-
+    console.log(stripeToken);
     const response = await stripe.charges.create({
       amount: req.fields.amount * 100,
       currency: "eur",
